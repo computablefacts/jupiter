4a47945b38ffa34ca671cf5f0e8365a3e66cf027
Cyrille Savelief <csavelief@mncc.fr>
2021-06-12 14:37:22 +0200

    Backport missing function to get users with system permissions or permissions on a given table.

--- src/com/computablefacts/jupiter/Users.java
+++ src/com/computablefacts/jupiter/Users.java
@@ -2,6 +2,7 @@ package com.computablefacts.jupiter;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
@@ -10,6 +11,7 @@ import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.security.ColumnVisibility;
+import org.apache.accumulo.core.security.SystemPermission;
 import org.apache.accumulo.core.security.TablePermission;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -194,6 +196,27 @@ final public class Users {
     return false;
   }
 
+  public static boolean grantPermission(Connector connector, String username,
+      SystemPermission permission) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(username),
+        "username should neither be null nor empty");
+    Preconditions.checkNotNull(permission, "permission should not be null");
+
+    SecurityOperations securityOperations = connector.securityOperations();
+
+    if (securityOperations != null) {
+      try {
+        securityOperations.grantSystemPermission(username, permission);
+        return true;
+      } catch (AccumuloSecurityException | AccumuloException e) {
+        logger_.error(LogFormatter.create(true).message(e).formatError());
+      }
+    }
+    return false;
+  }
+
   @Deprecated
   public static boolean revokeReadPermission(Connector connector, String username, String table) {
     return revokePermission(connector, username, table, TablePermission.READ);
@@ -227,6 +250,27 @@ final public class Users {
     return false;
   }
 
+  public static boolean revokePermission(Connector connector, String username,
+      SystemPermission permission) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkNotNull(permission, "permission should not be null");
+
+    SecurityOperations securityOperations = connector.securityOperations();
+
+    if (securityOperations != null) {
+      try {
+        if (hasPermission(connector, username, permission)) {
+          securityOperations.revokeSystemPermission(username, permission);
+        }
+        return true;
+      } catch (AccumuloSecurityException | AccumuloException e) {
+        logger_.error(LogFormatter.create(true).message(e).formatError());
+      }
+    }
+    return false;
+  }
+
   public static boolean hasPermission(Connector connector, String username, String table,
       TablePermission permission) {
 
@@ -247,5 +291,134 @@ final public class Users {
       }
     }
     return false;
+  }
+
+  public static boolean hasPermission(Connector connector, String username,
+      SystemPermission permission) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(username),
+        "username should neither be null nor empty");
+    Preconditions.checkNotNull(permission, "permission should not be null");
+
+    SecurityOperations securityOperations = connector.securityOperations();
+
+    if (securityOperations != null) {
+      try {
+        return securityOperations.hasSystemPermission(username, permission);
+      } catch (AccumuloSecurityException | AccumuloException e) {
+        logger_.error(LogFormatter.create(true).message(e).formatError());
+      }
+    }
+    return false;
+  }
+
+  public static boolean revokeAllTablePermissions(Connector connector, String username,
+      String table) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(username),
+        "username should neither be null nor empty");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(table),
+        "table should neither be null nor empty");
+
+    boolean isOk = revokePermission(connector, username, table, TablePermission.READ);
+    isOk = isOk && revokePermission(connector, username, table, TablePermission.WRITE);
+    isOk = isOk && revokePermission(connector, username, table, TablePermission.BULK_IMPORT);
+    isOk = isOk && revokePermission(connector, username, table, TablePermission.ALTER_TABLE);
+    isOk = isOk && revokePermission(connector, username, table, TablePermission.GRANT);
+    isOk = isOk && revokePermission(connector, username, table, TablePermission.DROP_TABLE);
+
+    return isOk;
+  }
+
+  public static boolean revokeAllSystemPermissions(Connector connector, String username) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(username),
+        "username should neither be null nor empty");
+
+    boolean isOk = revokePermission(connector, username, SystemPermission.GRANT);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.CREATE_TABLE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.DROP_TABLE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.ALTER_TABLE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.CREATE_USER);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.DROP_USER);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.ALTER_USER);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.SYSTEM);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.CREATE_NAMESPACE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.DROP_NAMESPACE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.ALTER_NAMESPACE);
+    isOk = isOk && revokePermission(connector, username, SystemPermission.OBTAIN_DELEGATION_TOKEN);
+
+    return isOk;
+  }
+
+  public static Set<String> usersWithTablePermissions(Connector connector, String table) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(table),
+        "table should neither be null nor empty");
+
+    return all(connector).stream().filter(user -> {
+      if (hasPermission(connector, user, table, TablePermission.READ)) {
+        return true;
+      }
+      if (hasPermission(connector, user, table, TablePermission.WRITE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, table, TablePermission.BULK_IMPORT)) {
+        return true;
+      }
+      if (hasPermission(connector, user, table, TablePermission.ALTER_TABLE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, table, TablePermission.GRANT)) {
+        return true;
+      }
+      return hasPermission(connector, user, table, TablePermission.DROP_TABLE);
+    }).collect(Collectors.toSet());
+  }
+
+  public static Set<String> usersWithSystemPermissions(Connector connector) {
+
+    Preconditions.checkNotNull(connector, "connector should not be null");
+
+    return all(connector).stream().filter(user -> {
+      if (hasPermission(connector, user, SystemPermission.GRANT)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.CREATE_TABLE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.DROP_TABLE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.ALTER_TABLE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.CREATE_USER)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.DROP_USER)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.ALTER_USER)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.SYSTEM)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.CREATE_NAMESPACE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.DROP_NAMESPACE)) {
+        return true;
+      }
+      if (hasPermission(connector, user, SystemPermission.ALTER_NAMESPACE)) {
+        return true;
+      }
+      return hasPermission(connector, user, SystemPermission.OBTAIN_DELEGATION_TOKEN);
+    }).collect(Collectors.toSet());
   }
 }
