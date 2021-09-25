0ff44cc81b5497a1a8d8c0bb35a120bf2287e78f
Cyrille Savelief <csavelief@mncc.fr>
2021-09-25 19:19:48 +0200

    Fix TravisCI build issue.

--- src/com/computablefacts/jupiter/Streams.java
+++ src/com/computablefacts/jupiter/Streams.java
@@ -5,6 +5,7 @@ import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 import com.google.errorprone.annotations.CheckReturnValue;
+import com.google.errorprone.annotations.Var;
 
 @CheckReturnValue
 final public class Streams {
@@ -12,8 +13,9 @@ final public class Streams {
   public static <T> void forEach(Stream<T> stream, BiConsumer<T, Breaker> consumer) {
 
     Spliterator<T> spliterator = stream.spliterator();
-    boolean hadNext = true;
     Breaker breaker = new Breaker();
+    @Var
+    boolean hadNext = true;
 
     while (hadNext && !breaker.shouldBreak()) {
       hadNext = spliterator.tryAdvance(elem -> consumer.accept(elem, breaker));
